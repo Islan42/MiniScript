75e012de96b25a73875e74da13b5b95596d436f2
Islan <islan12victhor@gmail.com>
2023-08-17 11:37:13 -0300

    Added ensure_csrf_cookie decorator

--- game/views.py
+++ game/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import  ensure_csrf_cookie(view)
 
+@ensure_csrf_cookie
 def index(request):
     return render(request, 'game/index.html')
